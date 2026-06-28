da0a1baeac2a6f50939114e73e5a3f1f7951b486
Temur Khaburdzania <T.Khaburdzania@smartsoft.ge>
2026-06-28 19:37:30 +0400

    leart functions, added discount calculator, and alert welcome message

--- content/js/script.js
+++ content/js/script.js
@@ -1,1 +1,16 @@
-console.log("hello nika")
+function calculateDiscountedPrice(price, discount) {
+    let discountAmount = price * discount / 100;
+    let finalPrice = price - discountAmount;
+
+    console.log(`Original Price: $${price}`);
+    console.log(`Discount: ${discount}%`);
+    console.log(`Discount Amount: $${discountAmount}`);
+    console.log(`Final Price: $${finalPrice}`);
+}
+
+calculateDiscountedPrice(1200, 20);
+
+function showWelcomeMessage() {
+    alert("Welcome to our Online Tech Shop!");
+}
+
