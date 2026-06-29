25b51cd1dfeb5fa4fa1be3e2729b06d950eef45b
Nikolozi-Tech <nikazhorzholiani6@gmail.com>
2026-06-29 22:27:47 +0400

    updated products

--- content/js/script.js
+++ content/js/script.js
@@ -14,22 +14,50 @@ function showWelcomeMessage() {
     alert("Welcome to our Online Tech Shop!");
 }
 
+ const products = [
+    {
+    name : "Macbook M3",
+    price : 2200,
+    category : "Laptop",
+    stock : 50,
+    isInStock : true
+    },
+    {
+        name: "Hyperx keyboard",
+        price: 200,
+        category: "keyboard",
+        stock: 100,
+        isInStock: true
+    },
+    {
+        name: "Hyperx Headset",
+        price: 150,
+        category: "headset",
+        stock: 99,
+        isInStock: true
+    },
+    {
+        name: "Samsung Odyssey Monitor",
+        price: 350,
+        category: "Monitor",
+        stock: 35,
+        isInStock: true
+    },
+    {
+        name: "Gaming Chair",
+        price: 400,
+        category: "Chair",
+        stock: 56,
+        isInStock: true
+    }
+];
 
-
- let productName = "Macbook M3";
- let productPrice = 2200;
- let productCategory = "Tech";
- let productStock = 50;
- let IsinStock = productStock > 0;
-
-console.log(products[0].name);
-console.log(products[0].price);
-console.log(products[0].category);
-console.log(products[0].stock);
-console.log(products[0].isInStock);
+for(let i = 0; i < products.length;i++){
+    console.log(products[i].name);
+}
 
 function checkProductAvailability(){
-    if(product.stock>0){
+    if(products[0].stock>0){
         console.log("Product is Available")
     }else{
         console.log("Product is not available")
@@ -39,7 +67,7 @@ function checkProductAvailability(){
 checkProductAvailability();
 
 function priceChecker() {
-    if (product.price > 1000) {
+    if (products[0].price > 1000) {
         console.log("პროდუქტი ძვირია");
     }
 }
@@ -51,7 +79,7 @@ function showProductDetails(name,price,stock){
     console.log("Product Stock:" + stock);
 }
 
-showProductDetails(productName,productPrice,productStock);
+showProductDetails(productsName,productPrice,productStock);
 
 function calculateTotalPrice(price, quantity) {
     let total = price * quantity;
