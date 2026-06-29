18581f0fd609c24775cb229efc60e2b5c95e1ee0
Nikolozi-Tech <nikazhorzholiani6@gmail.com>
2026-06-29 11:42:26 +0400

    added 3 functions , ProductAvailability , ShowDetails , Price Checker

--- content/js/script.js
+++ content/js/script.js
@@ -14,3 +14,48 @@ function showWelcomeMessage() {
     alert("Welcome to our Online Tech Shop!");
 }
 
+
+
+ let productName = "Macbook M3";
+ let productPrice = 2200;
+ let productCategory = "Tech";
+ let productStock = 50;
+ let IsinStock = productStock > 0;
+
+console.log(productName);
+console.log(productPrice);
+console.log(productCategory);
+console.log(productStock);
+console.log(isInStock);
+
+function checkProductAvailability(){
+    if(productStock>0){
+        console.log("Product is Available")
+    }else{
+        console.log("Product is not available")
+    }
+}
+
+checkProductAvailability();
+
+function priceChecker() {
+    if (productPrice > 1000) {
+        console.log("პროდუქტი ძვირია");
+    }
+}
+priceChecker();
+
+function showProductDetails(name,price,stock){
+    console.log("Product Name:" + name);
+    console.log("Product Price:" + price);
+    console.log("Product Stock:" + stock);
+}
+
+showProductDetails("Macbook",2200,50);
+
+function calculateTotalPrice(price, quantity) {
+    let total = price * quantity;
+    console.log("Total Price: " + total);
+}
+
+calculateTotalPrice(2200, 2);
